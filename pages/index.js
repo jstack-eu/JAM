2e5c7179362fb9de510a781e35033f948bf565e5
Jasper Baetens <jasper-baetens@bitbucket.org>
2021-04-09 14:35:46 +0200

    block test

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import theme from '../styles/theme';
+import {getEntry} from '../services/contentful';
+import {documentToReactComponents as renderRichText} from '@contentful/rich-text-react-renderer';
 
-const Home = () => {
+export async function getStaticProps() {
+  // Fetch all entries of content_type `blogPost`
+  const data = await getEntry('jHPWJqzEJasCbfzY8UD2n');
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+const Home = ({data}) => {
   console.log('1: ', process.env.CF_SPACE_ID);
-  // console.log('2: ', CF_SPACE_ID);
+  console.log('2: ', data);
+  console.log('3: ', data.fields);
+  console.log('4: ', data.fields.block);
+  console.log('5: ', data.fields.block[0].fields.title);
 
   return (
     <div className="container">
       <div className="home">
-        <div className="home-inner">
-          <div className="catchphrase">Hello world.</div>
-        </div>
+        <h1>{data.fields.block[0].fields.title}</h1>
+        {renderRichText(data.fields.block[0].fields.content)}
       </div>
 
       <style jsx>{`
--- services/contentful.js
+++ services/contentful.js
@@ -1,6 +1,26 @@
 import {createClient} from 'contentful';
 
-const client = createClient({
-  space: process.env.CF_SPACE_ID || 'gdied21sl5zt', // ID of a Compose-compatible space to be used \
-  accessToken: process.env.CF_DELIVERY_ACCESS_TOKEN || 'C9njcuLpIyB8haSePNkBXEg5rqH3kZyZoGjyiIptBcE', // delivery API key for the space \
-});
+let client;
+
+export const getClient = async () => {
+  if (client && client.geEntry) {
+    console.log('1');
+
+    return client;
+  }
+  console.log('2');
+  client = createClient({
+    space: process.env.CF_SPACE_ID || 'gdied21sl5zt', // ID of a Compose-compatible space to be used \
+    accessToken: process.env.CF_DELIVERY_ACCESS_TOKEN || 'C9njcuLpIyB8haSePNkBXEg5rqH3kZyZoGjyiIptBcE', // delivery API key for the space \
+    resolveLinks: true,
+  });
+  client.getEntries({content_type: 'page'}).then((response) => console.log('resp:', response.items));
+  client.getEntry('jHPWJqzEJasCbfzY8UD2n').then((response) => console.log('resp:', response));
+  return client;
+};
+
+export const getEntry = async (id) => {
+  const c = await getClient();
+  console.log('c: ', JSON.parse(JSON.stringify(c)));
+  return c.getEntry(id);
+};
